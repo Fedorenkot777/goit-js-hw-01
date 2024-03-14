3d355e68638ad82addfade5edde28936195ce0b5
Fedorenkot777 <155662260+Fedorenkot777@users.noreply.github.com>
2024-03-14 22:26:33 +0100

    js777999

--- js/task-2.js
+++ js/task-2.js
@@ -2,8 +2,7 @@ function getShippingMessage (country, price, deliveryFee) {
     const totalPrice = price + deliveryFee;
     return `Shipping to ${country} will cost ${totalPrice} credits!`;
     }
-    const country = "Australia";
-    const price = "price";
+    
 
     getShippingMessage("Australia", 120, 50);
     getShippingMessage("Germany", 80, 20);
