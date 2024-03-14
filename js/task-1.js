9edb0e9f1f711c2200ca68792a2eb059db542f4f
Fedorenkot777 <155662260+Fedorenkot777@users.noreply.github.com>
2024-03-14 22:08:23 +0100

    js799

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,13 @@
 function makeTransaction (quantity, pricePerDroid) {
-    "You ordered ${uantity},droids worth ${quantity * pricePerDroid} credits!";
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`; 
     }
     
     makeTransaction(5, 3000);
     makeTransaction(3, 1000);
     makeTransaction(10, 500);
 
-    // console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-    // console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 1000 credits!"
-    // console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 500 credits!"
+     // console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+     // console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 1000 credits!"
+     // console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 500 credits!"
    
