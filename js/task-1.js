101b722c90784a7ea3d2c148d3bb650bb2bb7170
Fedorenkot777 <155662260+Fedorenkot777@users.noreply.github.com>
2024-03-14 18:53:49 +0100

    js1

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function makeTransaction (quantity, pricePerDroid) {
+    "You ordered ${uantity},droids worth ${quantity * pricePerDroid} credits!";
+    }
+    
+    makeTransaction(5, 3000);
+    makeTransaction(3, 1000);
+    makeTransaction(10, 500);
+
+    // console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+    // console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 1000 credits!"
+    // console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 500 credits!"
+   
